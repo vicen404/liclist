be4366b705989378179257bcd49f5de4ec1b9300
vicen404 <vicentesanchez.alberto@gmail.com>
2020-09-15 16:06:46 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# liclist
+# Listado de licencias
+v1.0
+Script básico en shell para el listado de licencias.
+Con este script se podrá:
+- Obtener el número y tipo de licencias incluidas
+- Obtener el listado de las licencias no incluidas
+
+# A tener en cuenta
+Con la versión actual es posible que ocurra
+* No se obtengan correctamente las licencias debido a la version del fichero xml, o la versión del aplicativo.
+* El metodo de licenciamiento cambie y no se correspondan con la totalidad de las mismas
+
+
+# Modificaciones
+Inicialmente se obtenían tan solo las licencias incluidas, ahora se obtienen el total.
---
+++ source/listado_licencias.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+[ ! $1 ] && echo "Uso: traduce_licencias.sh <fichero.xml>" && exit 1
+
+echo "Licencias que aparecen en el fichero $1"
+echo -e "\nFormato de salida\n--------------------------------------"
+echo "#Cantidad - Descripción de la licencia"
+echo -e "\n\nOutput:"
+
+echo "Licencias incluidas"
+
+> license_name.tmp
+> license_quantity.tmp
+
+cat $1|grep " LicenseName="|egrep -v "Quantity=\"0\""|sed "s/<//g"|sed "s/\/>//g"|while read linea;do
+	tipo="LicenseName="
+	echo $linea|awk -F "\" " -v var=$tipo '{for (i=1; i<=NF; ++i) if ( match($i,var) ) {print $i}}' >> license_name.tmp
+	tipo="Quantity="
+	echo $linea|awk -F "\" " -v var=$tipo '{for (i=1; i<=NF; ++i) if ( match($i,var) ) {print $i}}' >> license_quantity.tmp
+done
+paste license_name.tmp license_quantity.tmp|awk -F"\"" '{print $3" - "$2}'|sed "s/Quantity=//g"|sed "s/-1/Ilimitadas/g"
+rm license_name.tmp license_quantity.tmp
+
+> license_name.tmp
+> license_quantity.tmp
+
+echo -e "\n\nLicencias NO incluidas:"
+
+cat $1|grep " LicenseName="|egrep "Quantity=\"0\""|sed "s/<//g"|sed "s/\/>//g"|while read linea;do
+	tipo="LicenseName="
+	echo $linea|awk -F "\" " -v var=$tipo '{for (i=1; i<=NF; ++i) if ( match($i,var) ) {print $i}}' >> license_name.tmp
+	tipo="Quantity="
+	echo $linea|awk -F "\" " -v var=$tipo '{for (i=1; i<=NF; ++i) if ( match($i,var) ) {print $i}}' >> license_quantity.tmp
+done
+paste license_name.tmp license_quantity.tmp|awk -F"\"" '{print "0 - "$2}'|sed "s/Quantity=//g"
+rm license_name.tmp license_quantity.tmp
